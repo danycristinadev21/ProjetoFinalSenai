0473e611119effc3d0d79e8a16e935eae74fd004
Danydev21 <danydcs@hotmail.com>
2025-12-05 02:15:39 -0300

    Ajustes nas telas de login, cadastro de usuário e menu, bem como desenvolvimento da tela de cadastro de produto

--- obj/Debug/net8.0/PapelArt.AssemblyInfo.cs
+++ obj/Debug/net8.0/PapelArt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PapelArt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b847b256ec48e2fab62f545e90b5ab2e89bc4c0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed1060e2541300f213d6a41fd2fa8390f8d6f51d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PapelArt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PapelArt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
