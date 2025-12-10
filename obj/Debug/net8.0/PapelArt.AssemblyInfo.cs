821f5686bf328d65c73e11dbcc4dc2e77b663f3b
Danydev21 <danydcs@hotmail.com>
2025-12-10 20:11:40 -0300

    Ajuste no logo do cabeçalho em todas as telas após a tela Menu, transformando-o em botão para voltar para a tela Menu

--- obj/Debug/net8.0/PapelArt.AssemblyInfo.cs
+++ obj/Debug/net8.0/PapelArt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PapelArt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915cff9c225cec3fd10a76ad700e01a8532bf900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac030eac1a8957b61390903e0c00cfdce6d62a3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PapelArt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PapelArt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
