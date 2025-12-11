3bff2d21f6f927f594d4db35be81620c8cd8a666
Danydev21 <danydcs@hotmail.com>
2025-12-10 23:49:43 -0300

    Implantação das telas para gerar e apresentar os relatórios, estilizando a tela de gerar relatórios, bem como realizando mais alguns ajustes gerais

---
+++ Controllers/RelatoriosController.cs
@@ -0,0 +1,84 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using PapelArt.Models;
+using PapelArt.Data;
+
+namespace PapelArt.Controllers
+{
+    public class RelatoriosController : Controller
+    {
+        private readonly PapelArtContext _context;
+
+        public RelatoriosController(PapelArtContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Periodos = new SelectList(new[]
+            {
+                "", "Hoje", "Ontem", "Esta Semana", "Este Mês", "Últimos 30 dias", "Todos"
+            }, null, "Selecione...");
+
+            ViewBag.Produtos = new SelectList(
+                _context.Produtos.Select(p => new { p.Id, p.Nome }).ToList(),
+                "Id",
+                "Nome",
+                ""
+            );
+
+            return View();
+        }
+
+        [HttpPost]
+        [HttpPost]
+        public async Task<IActionResult> Gerar(string periodo, int? produtoId)
+        {
+            IQueryable<Movimentacao> query = _context.Movimentacoes
+                .Include(m => m.Produto).ThenInclude(p => p.Categoria)
+                .Include(m => m.Usuario)
+                .OrderByDescending(m => m.DataMovimentacao);
+
+            if (!string.IsNullOrEmpty(periodo) && periodo != "Todos")
+            {
+                var dataFiltro = CalcularPeriodo(periodo);
+                query = query.Where(m => m.DataMovimentacao >= dataFiltro);
+            }
+
+            if (produtoId.HasValue && produtoId.Value > 0)
+                query = query.Where(m => m.ProdutoId == produtoId.Value);
+
+            var movimentacoes = await query.Take(200).ToListAsync();
+            return View("Create", movimentacoes);
+        }
+
+        private DateTime CalcularPeriodo(string periodo)
+        {
+            return periodo switch
+            {
+                "Hoje" => DateTime.Today,
+                "Ontem" => DateTime.Today.AddDays(-1),
+                "Esta Semana" => DateTime.Today.AddDays(-7),
+                "Este Mês" => new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1),
+                "Últimos 30 dias" => DateTime.Today.AddDays(-30),
+                _ => DateTime.MinValue
+            };
+        }
+
+        public IActionResult EstoqueAtual()
+        {
+            var estoque = _context.Produtos
+                .Include(p => p.Categoria)
+                .OrderBy(p => p.Categoria.Nome)
+                .ThenBy(p => p.Nome)
+                .ToList();
+            return View(estoque);
+        }
+    }
+}
+
+
